69ee5efbff5e26b05547f89793fd831a6305aeec
João Vitor Cotta <joaovitorcotta@gmail.com>
2021-08-15 12:57:07 -0300

    feat : create a base project

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import com.company.mainView.LoguinView;
+
+import javax.swing.*;
+
+public class Main  {
+
+    public static void main(String[] args) {
+        LoguinView loguinView = new LoguinView();
+
+        loguinView.mostraLoguin();
+    }
+
+}
---
+++ src/com/company/mainView/LoguinView.java
@@ -0,0 +1,30 @@
+package com.company.mainView;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoguinView {
+    private JPanel loguinPanel;
+    private JButton btnAdmin;
+    private JButton btnVeterinario;
+    private JButton btnVendedor;
+
+    public LoguinView() {
+        btnVendedor.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "rolou!!");
+            }
+        });
+
+    }
+
+    public void mostraLoguin() {
+        JFrame frame = new JFrame("Loguin");
+        frame.setContentPane(new LoguinView().loguinPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
