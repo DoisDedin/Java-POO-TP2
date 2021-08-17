04299d898c59e27198fdf4f8469ddebd9b2863fa
joao.cotta@usemobile.xyz <Eldemo1234>
2021-08-16 21:53:55 -0300

    feat: create a base MenuAddmin

--- src/com/company/ui/mainView/menus/admin/MenuAdminTela.java
+++ src/com/company/ui/mainView/menus/admin/MenuAdminTela.java
@@ -1,4 +1,14 @@
 package com.company.ui.mainView.menus.admin;
 
+import javax.swing.*;
+
 public class MenuAdminTela {
+    private JPanel menuPanel;
+    private JButton btnEstoque;
+    private JButton btnCliente;
+    private JButton btnFuncionarios;
+    private JButton btnContas;
+    private JButton btnProdutosServico;
+    private JButton btnVendas;
+    private JButton btnVoltar;
 }
