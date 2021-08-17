381e54b1741a82ed354c6adfba0e43466835059a
PatrickAlmeidaa <35865564+PatrickAlmeidaa@users.noreply.github.com>
2021-08-16 22:32:32 -0300

    telas de vendedor e veterinário criadas

--- src/com/company/ui/mainView/menus/vendedor/MenuVendedorTela.java
+++ src/com/company/ui/mainView/menus/vendedor/MenuVendedorTela.java
@@ -1,4 +1,10 @@
 package com.company.ui.mainView.menus.vendedor;
 
+import javax.swing.*;
+
 public class MenuVendedorTela {
+    private JPanel vendedorPanel;
+    private JButton btnCadastroCliente;
+    private JButton btnVendas;
+    private JButton btnVoltar;
 }
--- src/com/company/ui/mainView/menus/veterinario/MenuVeterinarioTela.java
+++ src/com/company/ui/mainView/menus/veterinario/MenuVeterinarioTela.java
@@ -1,4 +1,15 @@
 package com.company.ui.mainView.menus.veterinario;
 
+import javax.swing.*;
+
 public class MenuVeterinarioTela {
+    private JButton btnMostraClienre;
+    private JPanel panelVendedor;
+    private JButton btnTratamento;
+    private JButton btnOrdemServico;
+    private JButton btnVoltar;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
