32b520cf31ac8e6b7773a9deca7ca7c2644f76da
Erickjbf <erick.barroso@aluno.ufop.edu.br>
2021-08-23 21:30:36 -0300

    Construcao das subtelas do adm

--- src/com/company/ui/mainView/menus/admin/habilidades/AdicionarTela.java
+++ src/com/company/ui/mainView/menus/admin/habilidades/AdicionarTela.java
@@ -5,4 +5,8 @@ import javax.swing.*;
 public class AdicionarTela {
     private JComboBox comboBox1;
     private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton adicionarButton;
+    private JButton cancelarButton;
 }
