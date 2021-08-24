aa9cca2f81a38a08a45d17847472e93fc247797b
Erickjbf <erick.barroso@aluno.ufop.edu.br>
2021-08-24 16:53:14 -0300

    Finalizacao das subtelas do adm

--- src/com/company/ui/mainView/menus/admin/habilidades/PagarTela.java
+++ src/com/company/ui/mainView/menus/admin/habilidades/PagarTela.java
@@ -2,12 +2,11 @@ package com.company.ui.mainView.menus.admin.habilidades;
 
 import javax.swing.*;
 
-public class AdicionarFuncionarioTela {
+public class PagarTela {
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
-    private JTextField textField5;
-    private JButton cadastrarButton;
+    private JButton confirmarPagamentoButton;
     private JButton cancelarButton;
 }
---
+++ src/com/company/ui/mainView/menus/admin/habilidades/VendaServicoTela.java
@@ -0,0 +1,10 @@
+package com.company.ui.mainView.menus.admin.habilidades;
+
+import javax.swing.*;
+
+public class VendaServicoTela {
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JButton confirmarButton;
+    private JButton voltarButton;
+}
